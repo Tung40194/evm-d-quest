43988c73c79f59f18055bf7faee3e7f3ba9d0d02
tungpham <tungpham@lecle.co.kr>
2023-03-05 09:56:08 +0700

    generate docstring for MissionFormula lib (auditted gpt-generated docstring)

--- contracts/lib/MissionFormula.sol
+++ contracts/lib/MissionFormula.sol
@@ -3,25 +3,41 @@ pragma solidity 0.8.18;
 
 import "./DQuestStructLib.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-
-// basically missionformula but featured with quick deletion/reset
-
+/**
+ * @title MissionFormula
+ * @dev This library defines data structures and functions related to mission formulas.
+ */
 library MissionFormula {
+    
+    // Use EnumerableSet to manage node ids
     using EnumerableSet for EnumerableSet.UintSet;
 
-    // formula but no efficient reset feature
+    /**
+     * @dev Defines a formula data structure which stores mission nodes in a mapping.
+     * @param _values Mapping to store mission nodes.
+     * @param _keys EnumerableSet to manage node ids.
+     */
     struct Formula {
         mapping(uint256 => DQuestStructLib.MissionNode) _values;
-        // manage node ids
         EnumerableSet.UintSet _keys;
     }
 
-    // formula but featured with efficient reset
+    /**
+     * @dev Defines an efficiently resetable formula data structure which stores formulas in a mapping.
+     * @param erf Mapping to store formulas.
+     * @param rstPtr Pointer to the current formula in the mapping.
+     */
     struct efficientlyResetableFormula {
         mapping(uint256 => Formula) erf;
         uint256 rstPtr;
     }
 
+    /**
+     * @dev Adds nodes to the given formula and resets it.
+     * @param f Formula to add nodes to.
+     * @param nodes Array of mission nodes to add to the formula.
+     * @return Boolean indicating success.
+     */
     function _set(
         efficientlyResetableFormula storage f,
         DQuestStructLib.MissionNode[] memory nodes
@@ -39,6 +55,12 @@ library MissionFormula {
         }
     }
     
+    /**
+     * @dev Returns the mission node with the given id from the given formula.
+     * @param f Formula to get mission node from.
+     * @param nodeId Id of the mission node to get.
+     * @return Mission node with the given id.
+     */
     function _getNode(
         efficientlyResetableFormula storage f,
         uint256 nodeId
@@ -47,6 +69,10 @@ library MissionFormula {
         return f.erf[f.rstPtr]._values[nodeId];
     }
 
+    /**
+     * @dev Resets the given formula by incrementing the pointer to the next formula in the mapping.
+     * @param f Formula to reset.
+     */
     function _reset(efficientlyResetableFormula storage f) private {
         // inc pointer to reset mapping; omit id #0
         f.rstPtr ++;
