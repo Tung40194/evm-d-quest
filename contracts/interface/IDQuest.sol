9811af4660f8f13a9c8555b4350c1b2dc7749caa
Triet Dinh <trietdinh@lecle.co.kr>
2023-03-14 12:07:01 +0900

    Feat: chainlink mission handler (#4)

--- contracts/interface/IDQuest.sol
+++ contracts/interface/IDQuest.sol
@@ -4,7 +4,12 @@ pragma solidity 0.8.18;
 import "../lib/DQuestStructLib.sol";
 
 interface IDQuest {
-    function createQuest(DQuestStructLib.MissionNode[] memory missionFormular, DQuestStructLib.Outcome[] memory allOutcomes) external returns(address);
+    function createQuest(
+        DQuestStructLib.MissionNode[] memory missionFormular,
+        DQuestStructLib.Outcome[] memory allOutcomes
+    ) external returns (address);
+
     function getAllQuests() external view returns (address[] memory);
+
     function getQuests(address Quester) external view returns (address[] memory);
 }
