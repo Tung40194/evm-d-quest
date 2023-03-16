c9ad5b1328782c1ec5ed59b1d4fce0fefda2beb8
tungpham <tungpham@lecle.co.kr>
2023-03-16 09:39:13 +0700

    correct library visibility

--- contracts/lib/BytesConversion.sol
+++ contracts/lib/BytesConversion.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 // converting bytes to other types
 library BytesConversion {
-    function toUint256(bytes memory data) public pure returns (uint256) {
+    function toUint256(bytes memory data) internal pure returns (uint256) {
         uint256 result;
         assembly {
             result := mload(add(data, 32))
@@ -11,13 +11,13 @@ library BytesConversion {
         return result;
     }
 
-    function toAddress(bytes memory data) public pure returns (address addr) {
+    function toAddress(bytes memory data) internal pure returns (address addr) {
         assembly {
             addr := mload(add(data, 20))
         }
     }
 
-    function toString(bytes memory data) public pure returns (string memory) {
+    function toString(bytes memory data) internal pure returns (string memory) {
         return string(data);
     }
 }
