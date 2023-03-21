d2f96a6e79a3add46de0f6c2c3e1fde4d20d183b
tungpham <tungpham@lecle.co.kr>
2023-03-21 23:17:55 +0700

    prettier

--- contracts/lib/BytesConversion.sol
+++ contracts/lib/BytesConversion.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.17;
 // converting bytes to other types
 library BytesConversion {
     function toUint256(bytes memory data) internal pure returns (uint256) {
-        (uint256 decodedUint256) = abi.decode(data, (uint256));
+        uint256 decodedUint256 = abi.decode(data, (uint256));
         return decodedUint256;
     }
 
     function toAddress(bytes memory data) internal pure returns (address addr) {
-        (address decodedAddress) = abi.decode(data, (address));
+        address decodedAddress = abi.decode(data, (address));
         return decodedAddress;
     }
 
     function toString(bytes memory data) internal pure returns (string memory) {
-        (string memory decodedString) = abi.decode(data, (string));
+        string memory decodedString = abi.decode(data, (string));
         return decodedString;
     }
 }
